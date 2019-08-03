a1f7725f69f55703dbb41bbd6638df7553d232dd
George Bigham <gabigham4@gmail.com>
2019-08-03 11:20:12 -0700

    merged_data returns two data sets

--- merge_data.py
+++ merge_data.py
@@ -156,6 +156,10 @@ def merge_data():
     all_consump_data['energy']=11.96*all_consump_data['energy']
     all_consump_data['total_gen']=11.96*all_consump_data['total_gen']
 
+    #drop 
+    all_consump_data = all_consump_data.drop(columns = ['total_gen'])
+    
+    
     #filter power df and create df for cumulative capacity
     pow_pd = power_df[['country_long', 'capacity_mw', 'commissioning_year' ]]
     pow_pd.head(20)
